1f5bb2fdbc50b5b88f1c3ea9c8b1da00796c865c
Shayan Ahmad <shayanahmad235@gmail.com>
2024-06-09 22:28:14 +0500

    routes are created for login, register and dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [DashboardController::class, 'login'])->name('view.login');
+Route::get('/register', [DashboardController::class, 'register'])->name('view.register');
+Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('view.dashboard');
