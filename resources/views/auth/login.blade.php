c40ee616feb1f6c77ddd7e85486cec555850a3de
Shayan Ahmad <shayanahmad235@gmail.com>
2024-06-09 22:27:45 +0500

    login and register are done on the frontend side to login and register through api  using ajax

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ config('app.name') }} | Login</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+</head>
+
+<body>
+
+    <div class="flex items-center justify-center h-screen">
+        <div class="bg-white p-8 rounded shadow-md w-96">
+            <h2 class="text-xl font-semibold mb-6">Login</h2>
+            <div class="text-red-600" id="error"></div>
+            <form onsubmit="submitForm(this)" action="javascript:;" action_url="{{ route('login') }}" method="post"
+                class="max-w-sm mx-auto">
+                @csrf
+                <div class="mb-5">
+                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
+                        email</label>
+                    <input type="email" id="email" name="email"
+                        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                        placeholder="name@flowbite.com" required />
+                </div>
+                <div class="mb-5">
+                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
+                        password</label>
+                    <input type="password" name="password" id="password"
+                        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                        required />
+                </div>
+                <div class="flex items-start mb-5">
+                    <div class="flex items-center h-5">
+                        <input id="terms" type="checkbox" value=""
+                            class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                            required />
+                    </div>
+                    <label for="terms" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with
+                        the
+                        <a href="#" class="text-blue-600 hover:underline dark:text-blue-500">terms and
+                            conditions</a></label>
+                </div>
+                <button type="submit"
+                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
+                <div class="grid grid-cols-1 mt-2">
+                    <div class="col-span-3"></div>
+                    <div class="col-span-1 flex justify-end">
+                        <a href="{{ route('view.register') }}"
+                            class="text-blue-600 hover:underline dark:text-blue-500">Don't
+                            have an account?</a>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <script>
+        function submitForm(form) {
+            let url = $(form).attr('action_url');
+            let formData = $(form).serialize();
+            $.ajax({
+                type: 'POST',
+                url: url,
+                data: formData,
+                success: function(response) {
+                    try {
+                        // var obj = jQuery.parseJSON(JSON.stringify(response));
+                        if (response.message == 'success') {
+                            alert(response.message);
+                            localStorage.setItem('token', response.data.token);
+                            window.location.href = '/dashboard';
+                        } else {
+                            var error = 'Error: ' + response.message;
+                            $('error').html(error);
+                        }
+                    } catch (e) {
+                        alert('An error occurred: ' + e.message);
+                    }
+                }
+            });
+        }
+    </script>
+
+
+</body>
+
+</html>
